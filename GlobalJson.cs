d71093fdfc541fdc13d8ba4b520edc3478e7b2f4
David G. Moore, Jr <dev@dgmjr.io>
2023-04-01 21:33:41 -0500

    Update GlobalJson.cs

--- GlobalJson.cs
+++ GlobalJson.cs
@@ -1,5 +1,6 @@
 namespace Microsoft.Net.Global;
 
+using System.Collections;
 using System.Data;
 using System.Runtime.Serialization;
 using static System.String;
@@ -11,4 +12,8 @@ public record struct Json
     public Sdk? Sdk { get; set; } = null;
     [JProp("msbuild-sdks")]
     public IDictionary<string, Version> ProjectSdks { get; set; } = new Dictionary<string, Version>();
+    [JProp("projects")]
+    public IDictionary<string, DirectoryInfo> Projects { get; set; } = new Dictionary<string, DirectoryInfo>();
+    [JProp("tools")]
+    public IDictionary<string, Version> Tools { get; set; } = new Dictionary<string, Version>();
 }
